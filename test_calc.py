0cf19cab1c81c938ce11491d1f330008688bd596
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-08-12 21:06:05 +0200

    add test passed with OK!

--- test_calc.py
+++ test_calc.py
@@ -1,1 +1,9 @@
-#
+# our unittest testing.
+
+import unittest
+import calc
+
+class TestCalc(unittest.TestCase):
+    def test_add(self):
+        result = calc.add(10, 70)
+        self.assertEqual(result, 80)
